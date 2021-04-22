d7246d2855b0e1a83b2f12a9f57f50789760d093
Sasha <sasha@sasha.com>
2021-04-22 18:47:15 +0000

    Scraping

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import requests
 def get_articles():
     res = requests.get('https://news.ycombinator.com/')
     html_parser = BeautifulSoup(res.text, 'html.parser')
-    main_element = html_parser.select('#hnmain tr')[3:]
-    items_table = main_element[0].select('tr td table')
-    articles = items_table[0].select('tr .title .storylink')
-    points = items_table[0].select('tr .subtext .score')
+    main_element = html_parser.select('#hnmain tr')[3:]# getting table wrapper
+    items_table = main_element[0].select('tr td table')# getting all table elements
+    articles = items_table[0].select('tr .title .storylink')# getting articles' tags from the table
+    points = items_table[0].select('tr .subtext .score')# getting points' tags from the table
     article_hrefs = []
     
     for index, value in enumerate(articles[:len(articles)-1]):
         point_int = int(points[index].getText().split(' ')[0])
-        if point_int > 100:
+        if point_int > 100:# getting all articles with points greater than 100
             article_hrefs.append(articles[index].get('href'))
     return article_hrefs
 
